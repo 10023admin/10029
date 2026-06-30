5e648229cafcae025a6ac25c89d9eca24487b684
10023admin <ap130suxj@163.com>
2026-06-30 14:25:05 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://sywodzqshdktxpkynfip.supabase.co",
+  "sb_publishable_oFLVa6_MSTkoWzfGwRktdQ_7CJ8sccJ"
+);
